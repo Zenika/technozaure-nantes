547f887fce438627f1d1b77299e27a254888faef
Julien Landuré <julien.landure@gmail.com>
2018-09-24 17:03:30 +0200

    :sparkles: fixes talks

--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -3,11 +3,11 @@
 // return URL parameters from URL
 var getUrlParameter = function getUrlParameter(sParam) {
   var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-  sURLVariables = sPageURL.split('&'),
-  sParameterName,
-  i;
+    sURLVariables = sPageURL.split("&"),
+    sParameterName,
+    i;
   for (i = 0; i < sURLVariables.length; i++) {
-    sParameterName = sURLVariables[i].split('=');
+    sParameterName = sURLVariables[i].split("=");
     if (sParameterName[0] === sParam) {
       return sParameterName[1] === undefined ? true : sParameterName[1];
     }
@@ -16,87 +16,89 @@ var getUrlParameter = function getUrlParameter(sParam) {
 
 // check if start with http ou https
 function isUrl(url) {
-   return /^(f|ht)tps?:\/\//i.test(url);
+  return /^(f|ht)tps?:\/\//i.test(url);
 }
 
 // Get track class for the chip
 var getTrackColor = function getTrackColor(track) {
   switch (track) {
-    case 'web':
-      return 'color-bg-web devfest-chip';
-    case 'cloud':
-      return 'color-bg-cloud devfest-chip';
-    case 'mobile':
-      return 'color-bg-mobile devfest-chip';
-    case 'discovery':
-      return 'color-bg-discovery devfest-chip';
+    case "web":
+      return "color-bg-web devfest-chip";
+    case "cloud":
+      return "color-bg-cloud devfest-chip";
+    case "mobile":
+      return "color-bg-mobile devfest-chip";
+    case "discovery":
+      return "color-bg-discovery devfest-chip";
     default:
-      return 'color-bg-default devfest-chip';
+      return "color-bg-default devfest-chip";
   }
 };
 
 //  Get talk type class for the chip
 var getTypeColor = function getTypeColor(type) {
   switch (type) {
-    case 'conference':
-      return 'color-bg-conference devfest-chip';
-    case 'codelab':
-      return 'color-bg-codelab devfest-chip';
-    case 'quickie':
-      return 'color-bg-quickie devfest-chip';
+    case "conference":
+      return "color-bg-conference devfest-chip";
+    case "codelab":
+      return "color-bg-codelab devfest-chip";
+    case "quickie":
+      return "color-bg-quickie devfest-chip";
     default:
-      return 'color-bg-default devfest-chip';
+      return "color-bg-default devfest-chip";
   }
 };
 
 // Get the time label following the talk type
 var getTimeLabel = function getTimeLabel(type) {
   switch (type) {
-    case 'keynote':
-      return '50 min';
-    case 'repas':
-      return '1h30';
-    case 'conference':
-      return '50 min';
-    case 'codelab':
-      return '2 h';
-    case 'quickie':
-      return '20 min';
-    case 'after':
-      return '30 min';
-    case 'café':
-      return '15 min';
+    case "keynote":
+      return "50 min";
+    case "repas":
+      return "1h30";
+    case "conference":
+      return "40 min";
+    case "codelab":
+      return "2 h";
+    case "quickie":
+      return "20 min";
+    case "after":
+      return "30 min";
+    case "café":
+      return "15 min";
     default:
-      return '50 min';
+      return "50 min";
   }
 };
 
 getRoomLabel = function getRoomLabel(id) {
   switch (id) {
     case 1:
-      return language === 'en' ? 'Room Titan' : 'Salle Titan';
+      return language === "en" ? "Room Titan" : "Salle Titan";
     case 2:
-      return language === 'en' ? 'Room Belem' : 'Salle Belem';
+      return language === "en" ? "Room Belem" : "Salle Belem";
     case 3:
-      return language === 'en' ? 'Room Tour de Bretagne' : 'Salle Tour de Bretagne';
+      return language === "en"
+        ? "Room Tour de Bretagne"
+        : "Salle Tour de Bretagne";
     case 4:
-      return language === 'en' ? 'Room Graslin' : 'Salle Graslin';
+      return language === "en" ? "Room Graslin" : "Salle Graslin";
     case 5:
-      return language === 'en' ? 'Room Les Machines' : 'Salle Les machines';
+      return language === "en" ? "Room Les Machines" : "Salle Les machines";
     case 6:
-      return language === 'en' ? 'Room Tour LU' : 'Salle Tour LU';
+      return language === "en" ? "Room Tour LU" : "Salle Tour LU";
     case 7:
-      return language === 'en' ? 'Room le Château' : 'Salle le Château';
+      return language === "en" ? "Room le Château" : "Salle le Château";
     case 8:
-      return 'Mezzanine & Hall'
+      return "Mezzanine & Hall";
     default:
-      return 'Cité des congrès'
+      return "Cité des congrès";
   }
-}
+};
 
 // Get the day label
 var getDayLabel = function getDayLabel(day) {
-  return 'Vendredi 16 Mars'
+  return "Vendredi 16 Mars";
   // switch (day) {
   //   case 2:
   //     return language === 'en' ? 'Thursday, November 10th' : 'Jeudi 10 novembre';
@@ -113,9 +115,9 @@ var getHourLabel = function getHourLabel(day, hour, agenda) {
         return h.id === parseInt(hour);
       })[0].label;
     case 2:
-    return agenda.day2.filter(function(h) {
-      return h.id === parseInt(hour);
-    })[0].label;
+      return agenda.day2.filter(function(h) {
+        return h.id === parseInt(hour);
+      })[0].label;
   }
 };
 
@@ -123,92 +125,98 @@ var getHourLabel = function getHourLabel(day, hour, agenda) {
 var getDifficultyLabel = function getDifficultyLabel(difficulty) {
   switch (difficulty) {
     case 1:
-      return language === 'en' ? 'Level beginner' : 'Niveau débutant'
+      return language === "en" ? "Level beginner" : "Niveau débutant";
     case 2:
-      return language === 'en' ? 'Level intermediate' : 'Niveau intermédiaire'
+      return language === "en" ? "Level intermediate" : "Niveau intermédiaire";
     case 3:
-      return language === 'en' ? 'Level advanced' : 'Niveau avancé'
+      return language === "en" ? "Level advanced" : "Niveau avancé";
     default:
-      return language === 'en' ? 'Level beginner' : 'Niveau débutant'  
+      return language === "en" ? "Level beginner" : "Niveau débutant";
   }
 };
 
 // Get the language label
 var getLangLabel = function getLangLabel(lang) {
   switch (lang) {
-    case 'en':
-      return language === 'en' ? 'English' : 'Anglais'
+    case "en":
+      return language === "en" ? "English" : "Anglais";
     default:
-      return language === 'en' ? 'French' : 'Français'
+      return language === "en" ? "French" : "Français";
   }
 };
 
 var getSpeakers = function getSpeakers(speakersId, speakers) {
-  var label = '';
+  var label = "";
   for (var i = 0; i < speakersId.length; i++) {
     if (speakersId.length > 1 && i > 0 && i <= speakersId.length - 1) {
-      label += ' & '
+      label += " & ";
     }
     label += getSpeaker(speakersId[i], speakers);
   }
   return label;
-}
+};
 
 var getSpeaker = function getSpeaker(id, speakers) {
   var speaker = speakers.filter(function(s) {
-    return s.id === id
+    return s.id === id;
   })[0];
-  return getSpeakerLabel(speaker)
-}
+  return getSpeakerLabel(speaker);
+};
 
 var getSpeakerPhoto = function getSpeaker(id, speakers) {
   var speaker = speakers.filter(function(s) {
-    return s.id === id
+    return s.id === id;
   })[0];
-  return '/images/speakers/' + speaker.photo;
-}
+  return "/images/speakers/" + speaker.photo;
+};
 
 var getSpeakerLabel = function getSpeakerLabel(speaker) {
-  var company = speaker.company !== null ? ' (' + speaker.company + ')' : ''
-  return speaker.firstname + ' ' + speaker.name + company;
-}
+  var company = speaker.company !== null ? " (" + speaker.company + ")" : "";
+  return speaker.firstname + " " + speaker.name + company;
+};
 
 var getSessionByDayHour = function getSessionByDayHour(day, hour, sessions) {
   const s = sessions.filter(function(s) {
-    return s.agenda && s.agenda.day === day && s.agenda.hour === hour
+    return s.agenda && s.agenda.day === day && s.agenda.hour === hour;
   });
   s.sort(function(a, b) {
-    if (a.type === 'conference' && b.type !== 'conference') {
+    if (a.type === "conference" && b.type !== "conference") {
       return -1;
-    } else if (a.type !== 'conference' && b.type === 'conference') {
+    } else if (a.type !== "conference" && b.type === "conference") {
       return 1;
     } else {
       return 0;
     }
   });
   return s;
-}
+};
 
 // favorites
 var isFavorite = function(id, favs) {
   if (favs) {
-    return favs.indexOf(id.toString()) !== -1
+    return favs.indexOf(id.toString()) !== -1;
   } else {
-    return false
+    return false;
   }
-}
+};
 
-var toggleFavorite = function (id, favorite, favorites, userLogged) {
+var toggleFavorite = function(id, favorite, favorites, userLogged) {
   var index = favorites.indexOf(id.toString());
   if (index !== -1 && !favorite) {
     favorites.splice(index, 1);
   } else if (favorite) {
     favorites.push(id.toString());
   }
-  if (userLogged) { // waiting user login
-    fetch('/api/v1/stars/put?login=' + userLogged + '&favs=' + JSON.stringify(favorites)).then(function(response) {})
+  if (userLogged) {
+    // waiting user login
+    fetch(
+      "/api/v1/stars/put?login=" +
+        userLogged +
+        "&favs=" +
+        JSON.stringify(favorites)
+    ).then(function(response) {});
   } else {
-    localStorage['fav'] = JSON.stringify(favorites)
+    localStorage["fav"] = JSON.stringify(favorites);
   }
   return favorites;
-}
+};
